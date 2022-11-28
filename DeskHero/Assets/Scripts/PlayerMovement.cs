fe7c397d7957dfea82c2085a3d6e0df68c0dd79d
Abi Newcombe <abinewcombe098@gmail.com>
2022-11-28 20:41:58 +0000

    Add basic player movement

---
+++ DeskHero/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    private PlayerActions _playerActions;
+    private Rigidbody2D _rbody;
+    private Vector2 _moveInput;
+
+    private void Awake()
+    {
+        _playerActions = new PlayerActions();
+
+        _rbody = GetComponent<Rigidbody2D>();
+        if (_rbody is null)
+            Debug.LogError("Rigidbody2D is NULL!");
+    }
+
+    private void OnEnable()
+    {
+        _playerActions.Player_Map.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerActions.Player_Map.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        _moveInput = _playerActions.Player_Map.Move.ReadValue<Vector2>();
+        //_moveInput.x = 0f;
+        _rbody.velocity = _moveInput * _speed;
+    }
+
+}
